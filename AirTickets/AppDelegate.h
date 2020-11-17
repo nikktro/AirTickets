4ba2aaee47c3d0bc70cbc1722b60dabad983bd8f
Nikolay Trofimov <nikktro@gmail.com>
2020-11-17 18:29:56 +0300

    Initial Commit

---
+++ AirTickets/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AirTickets
+//
+//  Created by Nikolay Trofimov on 17.11.2020.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AirTickets/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AirTickets
+//
+//  Created by Nikolay Trofimov on 17.11.2020.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
